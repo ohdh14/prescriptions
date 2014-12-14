2b1c167dfae46469b9b0999f9f6f80ce437a1743
Matt Weller <wellermatt@hotmail.com>
2014-12-14 00:43:19 +0000

    Added R code

---
+++ geocode.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from pygeocoder import Geocoder as gc
+lat_lon = gc.geocode("KT229ET")
+lat = latitude
---
+++ pca_files.R
@@ -0,0 +1,28 @@
+library(data.table)
+require(stringr)
+
+files = grep(".csv|.CSV", dir("~/Data/prescriptions"),value = TRUE)
+SUBS = NULL
+BNFT = NULL
+
+myfread = function (fil)  {
+  print(fil)
+  dat = fread(paste0("~/data/prescriptions/" , fil))
+  period = substr(fil, 2, 7)
+  data.table(period = period, dat)[, lapply(.SD,str_trim)]
+}
+
+SUBS = rbindlist(lapply(grep("SUBS", files, value = TRUE ), myfread))
+BNFT = rbindlist(lapply(grep("ADDR BNFT", files, value = TRUE ), myfread))
+
+names.BNFT = c("V1", "period", "prac_id", "prac_name", "add_1", "add_2", "town", "county", "postcode")
+setnames(BNFT, names.BNFT)
+BNFT[, V1:=NULL]
+names.SUBS = c("period", "subs_id", "subs_name", "other")
+setnames(SUBS, names.SUBS)
+
+
+postcodes = data.table(postcode = unique(BNFT$postcode))
+#postcodes[, c("postcode_district", "postcode_area", "postcode_sector") := list(f)]
+postcodes[, `:=` (lat = 0, lon = 0)]
+postcodes
