d361150112a68b9184ccb09c792746ac522622dc
npetitclerc <npetitclerc@gmail.com>
2014-12-18 22:32:06 +0000

    Tuned parameters

--- find_anomaly.py
+++ find_anomaly.py
@@ -1,12 +1,14 @@
 import pandas as pd
-import utils.data_prep as data_prep
-import scikit-learn as sklearn
-import scikit-learn.covariance as cov
-import scipy.stats
+import data_prep
+import numpy as np
+import sklearn
+import scipy.stats as stats
 import matplotlib.pyplot as plt
+import sklearn.decomposition as decomposition
+import sklearn.covariance as cov
+#import sklearn.SVM as svm
 
-
-features = pd.read_csv('features_practice.csv')
+features = pd.read_csv('features_practice_all.csv')
 outliers_fraction = 0.25
 
 # Gaussify features
@@ -14,30 +16,72 @@ for col in features.columns:
   feat = features[col].values
   p = data_prep.gaussify(feat)
   print "P: ", p
-  feat = data_prep.apply_gauss_transform(feat, p):
+  feat = data_prep.apply_gauss_transform(feat, p)
   features[col] = feat
 
-clf = cov.EllipticEnvelope(contamination=.1)
+# Do PCA
+features = features.dropna().values
+#features = features.values
 
-clf.fit(features.values)
-y_pred = clf.decision_function(features.values).ravel()
-print y_pred
+pca = decomposition.PCA(n_components=2)
+features = pca.fit(features).transform(features)
+print features.shape
+
+# Gaussify features
+#for i_c, col in enumerate(features.T):
+#  #feat = features[col].values
+#  p = data_prep.gaussify(col)
+#  #print "P: ", p
+#  col = data_prep.apply_gauss_transform(col, p)
+#  features[:,i_c] = col
+
+
+clf = cov.EllipticEnvelope(contamination=0.001)
+
+clf.fit(features)
+y_pred = clf.decision_function(features).ravel()
 threshold = stats.scoreatpercentile(y_pred, 100 * outliers_fraction)
 y_pred = y_pred > threshold
 
 # Plotting:
-xx, yy = np.meshgrid(np.linspace(-7, 7, 500), np.linspace(-7, 7, 500))
+xx, yy = np.meshgrid(np.linspace(-3, 4, 500), np.linspace(-4, 2, 500))
 Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 
-plot = plt.plot() #subplot(1, 2, i + 1)
-plot.set_title("Outlier detection")
-plot.contourf(xx, yy, Z, levels=np.linspace(Z.min(), threshold, 7),
+plt.subplot(1, 2, 1)
+plt.title("Gaussian covariance Outlier detection")
+plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), threshold, 7),
                  cmap=plt.cm.Blues_r)
-a = plot.contour(xx, yy, Z, levels=[threshold],
+plt.contour(xx, yy, Z, levels=[threshold],
                     linewidths=2, colors='red')
-plot.contourf(xx, yy, Z, levels=[threshold, Z.max()],
+plt.contourf(xx, yy, Z, levels=[threshold, Z.max()],
                  colors='orange')
-b = plot.scatter(X[:-n_outliers, 0], X[:-n_outliers, 1], c='white')
-c = plot.scatter(X[-n_outliers:, 0], X[-n_outliers:, 1], c='black')
-plot.axis('tight')
+plt.scatter(features[:, 0], features[:, 1], c='black')
+plt.axis('tight')
+
+
+####
+clf = sklearn.svm.OneClassSVM(kernel="rbf", gamma=3, nu=0.95 * outliers_fraction + 0.05,)
+                                     #kernel="rbf", gamma=3)
+clf.fit(features)
+y_pred = clf.decision_function(features).ravel()
+threshold = stats.scoreatpercentile(y_pred, 100 * outliers_fraction)
+y_pred = y_pred > threshold
+
+# Plotting:
+xx, yy = np.meshgrid(np.linspace(-3, 4, 500), np.linspace(-4, 2, 500))
+Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.subplot(1, 2, 2)
+plt.title("SVM Outlier detection")
+plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), threshold, 7),
+                 cmap=plt.cm.Blues_r)
+plt.contour(xx, yy, Z, levels=[threshold],
+                    linewidths=2, colors='red')
+plt.contourf(xx, yy, Z, levels=[threshold, Z.max()],
+                 colors='orange')
+plt.scatter(features[:, 0], features[:, 1], c='black')
+plt.axis('tight')
+
+plt.show()
